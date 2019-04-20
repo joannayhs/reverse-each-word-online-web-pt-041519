ff34786842f1e8079af17cca1f8c5f21334ff98d
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 19:21:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def reverse_each_word(string)
  array = []
-  array << string.split.collect do |word|
+  array << string.split.each do |word|
     word.reverse!
   end
   array.join(" ")
